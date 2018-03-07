c4623d7be3cdfa2816bf0d050af2e70c9cac11b1
Joshua Magady <josh.magady@gmail.com>
2018-03-07 16:27:14 -0600

    feat(*): Implemented data binding on hero.name
    Implemented two way data binding on input. Added required deps to add the ngModel to the template engine.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,20 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'my-app',
   template: '''
   <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>
+  <div>
+    <label>id: </label>{{hero.id}}
+  </div>
+  <div>
+    <label>name: </label>{{hero.name}}
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
   ''',
+  directives: const [formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
