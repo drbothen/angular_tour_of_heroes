92fcd0433e25c4c7bfb5b6ed5ec40d9575e20e20
Joshua Magady <josh.magady@gmail.com>
2018-03-07 16:12:37 -0600

    feat(app_com): Added Hero Class

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,8 +2,17 @@ import 'package:angular/angular.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>',
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  var hero = 'Windstorm';
+}
+
+// Hero: This represent a hero in our app
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
 }
