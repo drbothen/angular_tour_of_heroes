fd527d9215300a0dbc0d4f6637bb4f9bce18f470
Joshua Magady <josh.magady@gmail.com>
2018-03-08 10:23:12 -0600

    feat(app_com): Moved template to its own file

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,17 +3,7 @@ import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div>
-    <label>id: </label>{{hero.id}}
-  </div>
-  <div>
-    <label>name: </label>{{hero.name}}
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
-  ''',
+  templateUrl: 'app_component.html',
   directives: const [formDirectives],
 )
 class AppComponent {
