158cc6b3af5957ac131316e32236672bb0f14a43
Joshua Magady <josh.magady@gmail.com>
2018-03-08 20:46:16 -0600

    feat(hero): Moved the hero class to its own file

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'src/hero.dart';
 
 @Component(
   selector: 'my-app',
@@ -9,12 +10,4 @@ import 'package:angular_forms/angular_forms.dart';
 class AppComponent {
   final title = 'Tour of Heroes';
   Hero hero = new Hero(1, 'Windstorm');
-}
-
-// Hero: This represent a hero in our app
-class Hero {
-  final int id;
-  String name;
-
-  Hero(this.id, this.name);
 }
---
+++ lib/src/hero.dart
@@ -0,0 +1,7 @@
+// Hero: This represent a hero in our app
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
