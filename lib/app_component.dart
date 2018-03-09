56a5ac1558db0b3a78d66140e6cee9bb37193b48
Joshua Magady <josh.magady@gmail.com>
2018-03-08 20:50:13 -0600

    feat(hero): Created Mock Heroes

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
+import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
@@ -9,5 +10,5 @@ import 'src/hero.dart';
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = new Hero(1, 'Windstorm');
+  List<Hero> hero = mockHeroes;
 }
