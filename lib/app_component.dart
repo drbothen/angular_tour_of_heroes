dce32234221e45976c0a491a992c62aaeeafd8a1
Joshua Magady <josh.magady@gmail.com>
2018-03-12 22:20:49 -0500

    feat(*): Converted Heros to Service
    Here’s what you achieved in this page:
    
    You created a service class that can be shared by many components.
    You used the ngOnInit lifecycle hook to get the hero data when the AppComponent activates.
    You defined the HeroService as a provider for the AppComponent.
    You designed the service to return a Future and the component to get the data from the Future.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,18 +2,29 @@ import 'package:angular/angular.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
 import 'src/hero_detail_component.dart';
+import 'src/hero_service.dart';
+import 'dart:async';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
   directives: const [CORE_DIRECTIVES, HeroDetailComponent],
+  providers: const [HeroService]
 )
-class AppComponent {
+class AppComponent implements OnInit {
+  void ngOnInit() => getHeroes();
+
   final title = 'Tour of Heroes';
+  final HeroService _heroService;
   List<Hero> heroes = mockHeroes;  // List of Hero
 
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroes();
+  }
+
   Hero selectedHero;  // Selected Hero
+  AppComponent(this._heroService);
   void onSelect(Hero hero) => selectedHero = hero; // Click event
 
 }
