ec1805edb6e9a3664a1dfe0bddeabc60e46d7d03
Joshua Magady <josh.magady@gmail.com>
2018-03-14 21:33:02 -0500

    feat(heroes): Added Hero Component

---
+++ lib/src/heroes_component.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:angular/angular.dart';
+import 'hero.dart';
+import 'hero_detail_component.dart';
+import 'hero_service.dart';
+@Component(
+  selector: 'my-heroes',
+  templateUrl: 'heroes_component.html',
+  styleUrls: const ['heroes_component.css'],
+  directives: const [CORE_DIRECTIVES, HeroDetailComponent],
+  providers: const [HeroService],
+)
+class HeroesComponent implements OnInit {
+  final title = 'Tour of Heroes';
+  final HeroService _heroService;
+  List<Hero> heroes;
+  Hero selectedHero;
+  HeroesComponent(
+      this._heroService
+      );
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroes();
+  }
+  void ngOnInit() => getHeroes();
+  void onSelect(Hero hero) => selectedHero = hero;
+}
